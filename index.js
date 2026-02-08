7a8a1f0039e998653a7d6ba9bd684fa78f0338ca
Gustavo <gugofogomes@gmail.com>
2026-02-07 23:31:52 -0300

    Correção de Bugs + Adicionar mecanicas de dados

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const DisplayChutes = document.getElementById("chute");
 const DisplayNumDados = document.getElementById("numDeDados");
-
+const DisplayDados = document.getElementById("dados");
 
 function modificarChute(quantidade){
     let novoValor = Number(DisplayChutes.textContent) + quantidade;
     let numDeDados = Number(DisplayNumDados.textContent);
 
-    if(novoValor >= 1 && novoValor <= 6*numDeDados){
-        DisplayChutes.textContent = novoValor;
-    }
+    if(novoValor >= 1 && novoValor <= 6*numDeDados) DisplayChutes.textContent = novoValor;
+    if(novoValor <= numDeDados) DisplayChutes.textContent = numDeDados;
 }
 
 function modificarDado(quantidade){
@@ -17,7 +16,15 @@ function modificarDado(quantidade){
 
     if(novoValor >= 1 && novoValor <= 6){
         if(chute > 6*novoValor) DisplayChutes.textContent = novoValor*6;
+        if(chute < novoValor) DisplayChutes.textContent = novoValor;
         DisplayNumDados.textContent = novoValor;
+        DisplayDados.innerHTML = " ";
+        for(let i = 1; i <= novoValor; i++){
+            console.log(i);
+            let ladoAleatorio = Math.floor(Math.random() * 6) + 1;
+            DisplayDados.innerHTML += `<img src="/imgs/dados/lado_${ladoAleatorio}.png" alt=" dado ne..." class="dado" id="dado${i}">`;
+            console.log(DisplayDados.innerHTML);
+        }
     }
 
 } 
